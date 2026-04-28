2f21d7061ffe2ea99c5d39b1098256b8ae9c37ce
ayoosh <alharazinaya@gmail.com>
2026-04-28 18:57:10 +0300

    Update StudentDAO to return a list of Student objects instead of Book objects

--- dao/StudentDAO.java
+++ dao/StudentDAO.java
@@ -12,15 +12,16 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import models.Book;
+import models.Student;
 
 /**
  *
  * @author aya
  */
 public class StudentDAO {
-    public static List<Book> findAllStudents(){
+    public static List<Student> findAllStudents(){
         Connection conn = DBConnection.getInstance().getConnection();
-        List<Book> books = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
         try {
             Statement stat = conn.createStatement();
             String sql = "SELECT * FROM students";
@@ -29,18 +30,14 @@ public class StudentDAO {
                 
                 int studentId = rs.getInt(1);
                 String name = rs.getString(2);
-                Student book = new Student(studentId,name);
-                books.add(book);
+                Student student = new Student(studentId,name);
+                students.add(student);
             }
         } catch (SQLException ex) {
             System.getLogger(BookDAO.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
         
         
-        return books;
-        
-        
-        
-        
+        return students;
     }
 }
