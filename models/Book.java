0c120408726432215f2b9ab8a75a3865a08e592d
ayoosh <alharazinaya@gmail.com>
2026-04-29 00:32:31 +0300

    Refactor Book class to remove genre, totalCopies, and availableCopies fields along with their associated methods, simplifying the class structure and focusing on essential attributes.

--- models/Book.java
+++ models/Book.java
@@ -11,9 +11,6 @@ package models;
 public class Book {
     private int bookId;
     private String title;
-    private String genre;
-    private int totalCopies;
-    private int availableCopies;
 
     
     public Book() {}
@@ -21,18 +18,12 @@ public class Book {
     public Book(String title,String genre,
             int totalCopies, int availableCopies) {
         this.title           = title;
-        this.genre           = genre;
-        this.totalCopies     = totalCopies;
-        this.availableCopies = availableCopies;
     }
 
     public Book(int bookId, String title,
                 String genre, int totalCopies, int availableCopies) {
         this.bookId          = bookId;
         this.title           = title;
-        this.genre           = genre;
-        this.totalCopies     = totalCopies;
-        this.availableCopies = availableCopies;
     }
 
 
@@ -42,14 +33,6 @@ public class Book {
     public String getTitle()                      { return title; }
     public void setTitle(String title)            { this.title = title; }
 
-    public String getGenre()                      { return genre; }
-    public void setGenre(String genre)            { this.genre = genre; }
-
-    public int getTotalCopies()                   { return totalCopies; }
-    public void setTotalCopies(int totalCopies)   { this.totalCopies = totalCopies; }
-
-    public int getAvailableCopies()               { return availableCopies; }
-    public void setAvailableCopies(int n)         { this.availableCopies = n; }
 
 
     @Override
@@ -57,9 +40,6 @@ public class Book {
         return "Book{" +
                "bookId="           + bookId          +
                ", title='"         + title           + '\'' +
-               ", genre='"         + genre           + '\'' +
-               ", totalCopies="    + totalCopies     +
-               ", availableCopies="+ availableCopies +
                "}\n";
     }
     
