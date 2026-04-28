16644dfdac63d4f2bc691043f8ec2fd1c044fe1d
ayoosh <alharazinaya@gmail.com>
2026-04-28 18:53:03 +0300

    Refactor BookDAO to return a list of Book objects with complete details from the database

--- dao/BookDAO.java
+++ dao/BookDAO.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,26 +20,27 @@ import java.sql.ResultSet;
 public class BookDAO {
     public static List<Book> findAllBooks(){
         Connection conn = DBConnection.getInstance().getConnection();
+        List<Book> books = new ArrayList<>();
         try {
             Statement stat = conn.createStatement();
             String sql = "SELECT * FROM books";
             ResultSet rs = stat.executeQuery(sql);
             while(rs.next()){
-                Book book = new Book();
+                
                 int bookId = rs.getInt(1);
                 String title = rs.getString(2);
                 String genre =rs.getString(3);
                 Integer totalCopies=rs.getInt(4);
-                
-                
-                
+                Integer availableCopies = rs.getInt(5);
+                Book book = new Book(bookId,title,genre,totalCopies,availableCopies);
+                books.add(book);
             }
         } catch (SQLException ex) {
             System.getLogger(BookDAO.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
         
         
-        
+        return books;
         
         
         
